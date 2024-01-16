ab8375f3fe0762565fa7db9deb7dff43ce954563
Baxtiyar Azimov <baxtiyar.azimov@aahm.lan>
2024-01-16 12:28:38 +0400

    add

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -1,15 +1,33 @@
 
+
+import Button from './components/button/button';
 import './App.css'
-import button from './components/button/button'
+
 
 function App() {
+const arr=[23,45,567];
+const obj = {
+  name:"John",
+  age:20
+};
 
-  return (
-    <div>
-    <div>Salam</div>
-    <button/>
-    </div>
-  )
+function handleClick() {
+  console.log("triggered");
 }
 
-export default App
+
+  return (
+<div>
+    <div>Salam</div>
+    <Button text="First" test="Bir" type="primary"
+     isActive={true} value={34} data={arr} personalinfo={obj}
+     handleClick={handleClick} />
+    <Button text="Second" test="Iki" type="secondary"/>
+    <Button text="Third" test=" Üç" type="primary"/>
+    </div>
+ 
+
+  );
+}
+
+export default App;
--- my_react_app/src/components/button/button.jsx
+++ my_react_app/src/components/button/button.jsx
@@ -1,10 +1,16 @@
 
-const button = () => {
-  return (
-    <div>
-      <button className="btn">Click</button>
-    </div>
-  )
-}
 
-export default button
+import "./button.css";
+
+const Button = ({text, test, type, data, personalinfo,handleClick})=> {
+    console.log(handleClick);
+    // const {text, test, type } =props;
+
+  return ( <button className={`btn ${type}`} onClick={handleClick}>
+  {test ? test : text}
+  </button>
+  );
+};
+
+export default Button;
+
